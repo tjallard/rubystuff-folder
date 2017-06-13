29a8f04c93ad239736a5ed49d70add34b4556ebd
TJ Allard <tjallard@gmail.com>
2017-06-13 13:28:12 -0400

    added 1 - 10 loop

--- TJ_Ruby_1.rb
+++ TJ_Ruby_1.rb
@@ -1,5 +1,17 @@
 counter = 1
-while counter < 22
-puts counter
-counter +=1
+	while counter < 22
+	puts counter
+	counter += 1
 end
+
+
+m = 1
+loop do |m|
+	m += 1
+	print m
+	break if m == 10	
+end
+
+
+
+
